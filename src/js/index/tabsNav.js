d528cc5d92ca541263b979c66173f8b39fe1dc4b
songwx <songwuxin@live.cn>
2018-07-04 12:48:44 +0800

    more

--- src/js/index/tabsNav.js
+++ src/js/index/tabsNav.js
@@ -12,12 +12,21 @@
 			this.view = view
 			this.view.init()
 			this.bindEvents()
+			this.bindEventHub()
 		},
 		bindEvents(){
 			this.view.$el.on('click','li',(e)=>{
 				$(e.currentTarget).addClass('active')
 					.siblings()
 					.removeClass('active')
+			})
+		},
+		bindEventHub(){
+			this.view.$el.on('click','li',(e)=>{
+				let tabName = $(e.currentTarget)
+					.find('div')
+					.attr('data-tab-name')
+				window.eventHub.emit('selectTab',tabName)
 			})
 		}
 	}
