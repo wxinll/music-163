8282438f279bc15716aa792dfea4d6a8111ae674
songwx@PC <songwuxin@live.cn>
2018-08-07 19:58:24 +0800

    hotsong&search

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -17,10 +17,6 @@
       this.view = view 
       this.view.init()
       this.model = model
-      // this.bindEventHub()
-      // this.loadModule1()
-      // this.loadModule2()
-      //代码没啥呀……
     },
     bindEventHub(){
       window.eventHub.on('selectTab', (tabName)=>{
@@ -30,25 +26,7 @@
           this.view.hide()
         }
       })
-    },
-    loadModule1(){
-      let script1 = document.createElement('script')
-      script1.src = './js/index/page-1-1.js' 
-      script1.onload = function(){
-        console.log('模块一加载完毕')
-      }
-      document.body.appendChild(script1)
-
-    },
-    loadModule2(){
-      let script2 = document.createElement('script')
-      script2.src = './js/index/page-1-2.js' 
-      script2.onload = function(){
-        console.log('模块2加载完毕')
-      }
-      document.body.appendChild(script2)
     }
-
   }
   controller.init(view, model)
 }
